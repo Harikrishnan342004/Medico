85ac1b188351391193f9c917aec28d9732c8ba4e
Hari <harikrishnan53828@gmail.com>
2023-09-14 14:55:01 +0530

    also

---
+++ Script/Signup-front.js
@@ -0,0 +1,24 @@
+function Myfunction(){
+
+
+var x = document.getElementById("password");
+if(x.type === "password"){
+    x.type = "test";
+}
+else{
+    x.type = "password";
+}
+
+}
+function myfunction(){
+
+
+    var x = document.getElementById("Password");
+    if(x.type === "password"){
+        x.type = "test";
+    }
+    else{
+        x.type = "password";
+    }
+    
+    }
